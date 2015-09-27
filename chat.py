321d1cd48b7cfccfcf659201d76a3b0e4f71519b
AnishN <anish.narayanan32@gmail.com>
2015-09-27 00:03:24 -0400

    pointless

--- chat.py
+++ chat.py
@@ -3,7 +3,25 @@ from gevent import monkey
 monkey.patch_all()
 from app import create_app, socketio
 
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+from flask import jsonify
+
 app = create_app(True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+    
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
 
 if __name__ == '__main__':
     socketio.run(app)
